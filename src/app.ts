32e7e332e83a34547c83d7564ea3c8f328af6861
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-05-06 13:32:44 -0300

    chore: Update app.ts to use environment variable for port
    This commit updates the `app.ts` file to use the environment variable `PORT` for the server port instead of hardcoding it to `3000`. This change allows for more flexibility in configuring the server port and aligns with best practices for deploying applications.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import 'dotenv/config';
 import { PlayerAuth_router } from "./routes/playerAuthRoute";
 import { player_router } from "./routes/PlayerInfoRoute";
 import { DodgeQueueRouter } from "./routes/dodgeQueueRoute";
 import { ClientInfo_Router } from "./routes/RiotClientInfo";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5110;
 
 app.use(express.json());
 app.use('/api/riot', PlayerAuth_router);
