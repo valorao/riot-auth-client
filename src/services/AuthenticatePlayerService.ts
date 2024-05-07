85bc425f7b1bda7158f558525ea948032263dc87
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-05-07 04:05:04 -0300

    refactor: Update AuthenticatePlayerService to handle authentication errors
    This commit updates the `AuthenticatePlayerService.ts` file to handle authentication errors. Previously, the code only returned the response status and error message when authentication failed. This change adds a conditional check for a successful response status (200) and includes additional data such as the `ssid` cookie, `riotid`, `puuid`, `token`, `expires`, `id_token`, and `entitlements_token` when authentication is successful. This improves the error handling and provides more comprehensive information in the response.

--- src/services/AuthenticatePlayerService.ts
+++ src/services/AuthenticatePlayerService.ts
@@ -12,41 +12,45 @@ const getPlayerInfo = new GetPlayerInfo();
 
 export class AuthenticatePlayerService {
     handle = async (username: string, password: string) => {
-        const cookiesValue = await createCookie.handle();
-        const version = await getClientVersion.ClientVersion();
-        const clientversion = version.data.data.version;
-        const response = await authAccount.AuthCookies(
-            clientversion , username, password, cookiesValue || '').catch(err => {
-                return err.response;
-            });
-        const uri = response.response.data.response.parameters.uri;
-        const url = new URL(uri);
-        const params = new URLSearchParams(url.search);
-        const token = params.get('access_token');
-        const expires = params.get('expires_in');
-        const id_token = params.get('id_token');
-    
-        const ent = await getEntitlements.Entitlements(token || '').catch(err => {
-            return err.response;
-        });
-        const entitlements_token = ent.data.entitlements_token;
-    
-        const info = await getPlayerInfo.PlayerInfo(token || '').catch(err => {
-            return err.response;
-        });
-        const ssid = response.ssid_cookie
-        const puuid = info.data.sub;
-        const riotid = info.data.acct.game_name + '#' + info.data.acct.tag_line;
-        response.puuid = [
-            {
-                name: 'puuid',
-                value: puuid,
-                options: {
-                    httpOnly: true,
-                }
+            const cookiesValue = await createCookie.handle();
+            const version = await getClientVersion.ClientVersion();
+            const clientversion = version.data.data.version;
+            const response = await authAccount.AuthCookies(
+                clientversion , username, password, cookiesValue || '').catch(err => {
+                    return err.response;
+                });
+            if (response.status === 200) {
+                const uri = response.response.data.response.parameters.uri;
+                const url = new URL(uri);
+                const params = new URLSearchParams(url.search);
+                const token = params.get('access_token');
+                const expires = params.get('expires_in');
+                const id_token = params.get('id_token');
+            
+                const ent = await getEntitlements.Entitlements(token || '').catch(err => {
+                    return err.response;
+                });
+                const entitlements_token = ent.data.entitlements_token;
+            
+                const info = await getPlayerInfo.PlayerInfo(token || '').catch(err => {
+                    return err.response;
+                });
+                const ssid = response.ssid_cookie
+                const puuid = info.data.sub;
+                const riotid = info.data.acct.game_name + '#' + info.data.acct.tag_line;
+                response.puuid = [
+                    {
+                        name: 'puuid',
+                        value: puuid,
+                        options: {
+                            httpOnly: true,
+                        }
+                    }
+                ];
+                return { status: response.response.status, cookie: response.puuid, riotid, puuid, token, expires, id_token, entitlements_token, ssid };
             }
-        ];
-        return { status: response.response.status, cookie: response.puuid, riotid, puuid, token, expires, id_token, entitlements_token, ssid };
+            else {
+                return { status: response.response.status, error: response.response.data };
+            }
     }
 }
-
