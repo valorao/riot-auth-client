5d4491950cb6004183a8b3ac7dd8d6cb690afe09
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-05-07 01:36:55 -0300

    refactor: Remove console.log statement in ReauthCookiesService handle method

--- src/services/ReauthCookiesService.ts
+++ src/services/ReauthCookiesService.ts
@@ -5,7 +5,6 @@ const getEntitlements = new GetEntitlements();
 
 export class ReauthCookiesService {
     handle = async (ssid: string) => {
-        console.log(ssid)
         const url = `https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&scope=account%20openid}`
         const config = {
             headers: {
