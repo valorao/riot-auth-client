1051f6d69943833f15e91524345190b91ba73a7f
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-05-13 20:33:26 -0300

    chore: Update fetch URL for checkcookies service

--- src/public/pt-BR/services/checkcookies.js
+++ src/public/pt-BR/services/checkcookies.js
@@ -8,7 +8,10 @@ export function checkCookies() {
         const player_banner_img = document.getElementById('player-banner-img');
         const page_title = document.getElementById('page-title')
         const dodgeBtn = document.getElementById('dodgeBtn');
-        fetch('https://apis.valorao.cloud/rso/fromstatic/cookies')
+        fetch('https://apis.valorao.cloud/rso/fromstatic/cookies', {
+            method: 'GET',
+            credentials: 'include',
+        })
         .then(response => response.json())
         .then(data => {
             if (data.puuid && data.ssid) {
