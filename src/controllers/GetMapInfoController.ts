0f49f61fdd67d264590bc381b57363fe777d110c
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-07-08 17:01:20 -0300

    Update MapInfo to use queries and not require authentication anymore.

--- src/controllers/GetMapInfoController.ts
+++ src/controllers/GetMapInfoController.ts
@@ -3,21 +3,15 @@ import GetMapInfo from '../services/GetMapInfo';
 const getMapInfo = new GetMapInfo();
 
 export const MapInfo = async (req: Request, res: Response) => {
-    if (!req.body.mapUrl) {
+    if (!req.query.mapUrl || !req.query.mapName) {
         return res.status(400).json({
-            "status": 400,
-            "error": "Bad Request",
-            "message": "missing header parameters"
+            status: 400,
+            error: "Bad Request",
+            message: "missing query parameters"
         });
     }
     const response = await getMapInfo.handle(
         (req.query.mapUrl as string), (req.query.mapName as string)
     );
-    if (response.status === 400) {
-        return res.status(401).json({
-            "status": 401,
-            "message": "Please relogin or reauth cookies."
-        })
-    }
-    res.status(200).json(response)
+    return res.status(200).json(response)
 }
